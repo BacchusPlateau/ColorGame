0f75805e9d79b3ce95485017a9eb46fb28adbc2e
Bret Williams <straightener@hotmail.com>
2020-04-07 17:41:00 -0500

    weird git stuff

--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -9,77 +9,234 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+enum Enemies: Int {
+    case small
+    case medium
+    case large
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var tracksArray: [SKSpriteNode]? = [SKSpriteNode]()
+    var player: SKSpriteNode?
+    var target: SKSpriteNode?
+    
+    var currentTrack = 0
+    var movingToTrack = false
+    
+    let moveSound = SKAction.playSoundFileNamed("move.wav", waitForCompletion: false)
+    
+    let trackVelocities = [180, 200, 250]
+    var directionArray = [Bool]()
+    var velocityArray = [Int]()
+    
+    var playerCategory: UInt32 = 0x1 << 0
+    var enemyCategory: UInt32 = 0x1 << 1
+    var targetCategory: UInt32 = 0x1 << 2
+    
+    func createEnemy(type: Enemies, forTrack track:Int) -> SKShapeNode? {
+        
+        let enemySprite = SKShapeNode()
+        
+        switch type {
+        case .small:
+            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 70),
+                                      cornerWidth: 8, cornerHeight: 8, transform: nil)
+            enemySprite.fillColor = UIColor(red: 0.4431, green: 0.5529, blue: 0.7451, alpha: 1)
+        case .medium:
+            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 100),
+                                      cornerWidth: 8, cornerHeight: 8, transform: nil)
+            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.4039, blue: 0.4039, alpha: 1)
+        case .large:
+            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 130),
+                                      cornerWidth: 8, cornerHeight: 8, transform: nil)
+            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.6392, blue: 0.4039, alpha: 1)
+        }
+        
+        guard let enemyPosition = tracksArray?[track].position else { return nil }
+        
+        let up = directionArray[track]
+                
+        enemySprite.name = "ENEMY"
+        enemySprite.position.x = enemyPosition.x
+        enemySprite.position.y = up ? -130 : self.size.height + 130
+        enemySprite.physicsBody = SKPhysicsBody(edgeLoopFrom: enemySprite.path!)
+        enemySprite.physicsBody?.categoryBitMask = enemyCategory
+        enemySprite.physicsBody?.velocity = up ? CGVector(dx: 0, dy: velocityArray[track]) : CGVector(dx: 0, dy: -velocityArray[track])
+        
+        return enemySprite
+        
+    }
+    
+    func createPlayer() {
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player?.physicsBody = SKPhysicsBody(circleOfRadius: player!.size.width / 2)
+        player?.physicsBody?.linearDamping = 0
+        player?.physicsBody?.categoryBitMask = playerCategory
+        player?.physicsBody?.collisionBitMask = 0  //deactivates all collisions
+        player?.physicsBody?.contactTestBitMask = enemyCategory | targetCategory
+        
+        guard let playerPosition = tracksArray?.first?.position.x else {
+            return
+        }
+        player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
+        self.addChild(player!)
+        
+        let pulse = SKEmitterNode(fileNamed: "pulse.sks")!
+        player?.addChild(pulse)
+        pulse.position = CGPoint(x: 0, y: 0)
+        
+    }
+    
+    func createTarget() {
+        
+        target = self.childNode(withName: "target") as? SKSpriteNode
+        target?.physicsBody = SKPhysicsBody(circleOfRadius: target!.size.width / 2)
+        target?.physicsBody?.categoryBitMask = targetCategory
+        target?.physicsBody?.collisionBitMask = 0
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        var playerBody: SKPhysicsBody
+        var otherBody: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            playerBody = contact.bodyA
+            otherBody = contact.bodyB
+        } else {
+            playerBody = contact.bodyB
+            otherBody = contact.bodyA
+        }
+        
+        //question: is this needed below?
+        //if playerBody.categoryBitMask == playerCategory
+        // ?  because when would the playerBody ever not have a category bitmask equal to the playerCategory?
+        
+        if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == enemyCategory {
+            print("Enemy hit")
+        } else if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == targetCategory  {
+            print("Target hit")
+        }
+    }
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        setupTracks()
+        createPlayer()
+        createTarget()
+        
+        self.physicsWorld.contactDelegate = self
+        
+        if let numberOfTracks = tracksArray?.count {
+            for _ in 0 ... numberOfTracks {
+                let randomNumberForVelocity = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+                velocityArray.append(trackVelocities[randomNumberForVelocity])
+                directionArray.append(GKRandomSource.sharedRandom().nextBool())
+            }
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.run(SKAction.repeatForever(SKAction.sequence([SKAction.run {
+            self.spawnEnemies()
+            }, SKAction.wait(forDuration: 2)])))
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    }
+    
+    func moveToNextTrack() {
+        
+        player?.removeAllActions()
+        movingToTrack = true
+        
+        guard let nextTrack = tracksArray?[currentTrack + 1].position else { return }
+        
+        if let player = self.player {
+            let moveAction = SKAction.move(to: CGPoint(x: nextTrack.x, y: player.position.y), duration: 0.2)
+            player.run(moveAction, completion: {
+                self.movingToTrack = false
+            })
+            currentTrack += 1
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            self.run(moveSound)
         }
+        
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    func moveVertically (up: Bool) {
+        
+        var yMovement:CGFloat = -3
+        
+        if up {
+            yMovement = 3
         }
+        
+        let moveAction = SKAction.moveBy(x: 0, y: yMovement, duration: 0.01)
+        let repeatAction = SKAction.repeatForever(moveAction)
+        player?.run(repeatAction)
+        
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    func setupTracks() {
+        
+        for i in 0 ... 8 {
+            if let track = self.childNode(withName: "\(i)") as? SKSpriteNode {
+                tracksArray?.append(track)
+            }
         }
+        
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+    func spawnEnemies() {
+        
+        for i in 1 ... 7 {
+            
+            let randomEnemyType = Enemies(rawValue: GKRandomSource.sharedRandom().nextInt(upperBound: 3))!
+            if let newEnemy = createEnemy(type: randomEnemyType, forTrack: i) {
+                self.addChild(newEnemy)
+            }
+            
         }
+        
+        //remove enemies that are no longer visible
+        self.enumerateChildNodes(withName: "ENEMY") { (node: SKNode, nil) in
+            if node.position.y < -150 || node.position.y > self.size.height + 150 {
+               node.removeFromParent()
+            }
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        
+        if let touch = touches.first {
+            let location = touch.previousLocation(in: self)
+            let node = self.nodes(at: location).first
+            
+            if node?.name == "right" {
+                moveToNextTrack()
+            } else if node?.name == "up" {
+                moveVertically(up: true)
+            } else if node?.name == "down" {
+                moveVertically(up: false)
+            }
+            
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+        player?.removeAllActions()
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if !movingToTrack {
+            player?.removeAllActions()
+        }
+        
     }
     
     
